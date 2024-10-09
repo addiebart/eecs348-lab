59ed455a8690487e615a09753a7c47c80caa7245
Addie Bart <addiebart@ku.edu>
2024-10-08 23:52:43 -0500

    implement string find function

--- Lab-5/SalesReport.c
+++ Lab-5/SalesReport.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+char* getMonthStr(int month) {
+    char* strings[] = {
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    };
+
+    return strings[month];
+}
+
 int main() {
     float figures[12];
     float hold;
@@ -11,44 +30,8 @@ int main() {
     printf("Monthly Sales Report for 2024:\n\n");
     printf("Month\tSales\n");
     for (int i = 0; i < 12; i++) {
-        switch (i) {
-            case 0:
-                printf("%s:\t$%,2f\n", "January", figures[i]);
-                break;
-            case 1:
-                printf("%s:\t$%,2f\n", "February", figures[i]);
-                break;
-            case 2:
-                printf("%s:\t$%,2f\n", "March", figures[i]);
-                break;
-            case 3:
-                printf("%s:\t$%,2f\n", "April", figures[i]);
-                break;
-            case 4:
-                printf("%s:\t$%,2f\n", "May", figures[i]);
-                break;
-            case 5:
-                printf("%s:\t$%,2f\n", "June", figures[i]);
-                break;
-            case 6:
-                printf("%s:\t$%,2f\n", "July", figures[i]);
-                break;
-            case 7:
-                printf("%s:\t$%,2f\n", "August", figures[i]);
-                break;
-            case 8:
-                printf("%s:\t$%,2f\n", "September", figures[i]);
-                break;
-            case 9:
-                printf("%s:\t$%,2f\n", "October", figures[i]);
-                break;
-            case 10:
-                printf("%s:\t$%,2f\n", "November", figures[i]);
-                break;
-            case 11:
-                printf("%s:\t$%,2f\n", "December", figures[i]);
-                break;
-        }
+        char* monthStr = getMonthStr(i);
+        printf("%s:\t$%,2f\n", monthStr, figures[i]);
     }
 
     printf("\nSales Summary Report:\n\n");
@@ -70,8 +53,8 @@ int main() {
         average += figures[i]; 
     }
     average /= 12;
-    printf("%s\t$%.2f\n", "Minimum Sales:", min);
-    printf("%s\t$%.2f\n", "Maximum Sales:", max);
+    printf("%s\t$%.2f\t(%s)\n", "Minimum Sales:", min, getMonthStr(mini));
+    printf("%s\t$%.2f\t(%s)\n", "Maximum Sales:", max, getMonthStr(maxi));
     printf("%s\t$%.2f\n\n", "Average Sales:", average);
 
     return 0;
