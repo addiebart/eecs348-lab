d00405b70dd0149553c7a5f8381eddcb64d3fa28
Addison Bartelli <addiebart@ku.edu>
2024-10-30 19:43:38 -0500

    provided files changed

--- Lab-7/main.cpp
+++ Lab-7/main.cpp
@@ -3,66 +3,86 @@
 
 using namespace std;
 
-const int MAX_SIZE = 4; // Adjust if necessary
+class Matrix {
+public:
+    int matrix_size;
+    double** matrix_data;
 
-void read_matrix_from_stream(double matrix_1[][MAX_SIZE], double matrix_2[][MAX_SIZE], int &matrix_size) {
+    Matrix(int size = 0) : matrix_size(size) {
+        matrix_data = new double*[matrix_size];
+        for (int i = 0; i < matrix_size; ++i) {
+            matrix_data[i] = new double[matrix_size];
+        }
+    }
 
-  cout << "Enter the size of the matrices: ";
-  cin >> matrix_size;
+    ~Matrix() {
+        for (int i = 0; i < matrix_size; ++i) {
+            delete[] matrix_data[i];
+        }
+        delete[] matrix_data;
+    }
+};
 
-  // TODO: Read matrix data from cin into matrix_1 and matrix_2
-  //       Make sure to handle potential errors and invalid input
-
+void read_matrix_from_file(const string& file_name, Matrix& matrix_1, Matrix& matrix_2) {
+    // TODO: Read matrix data from the file
+    //    Make sure to handle potential errors and invalid input
 }
 
-void print_matrix(const double matrix[][MAX_SIZE], int matrix_size) {
-  // TODO: Print the matrix to the console
+void print_matrix(const Matrix& matrix) {
+    // TODO: Print the matrix to the console
 }
 
-void print_matrix(const double matrix_1[][MAX_SIZE], const double matrix_2[][MAX_SIZE], int matrix_size) {
-  // TODO: Print both matrices to the console
+void print_matrix(const Matrix& matrix_1, const Matrix& matrix_2) {
+    // TODO: Print both matrices to the console
 }
 
-void add_matrices(const double matrix_1[][MAX_SIZE], const double matrix_2[][MAX_SIZE], double result[][MAX_SIZE], int matrix_size) {
-  // TODO: Implement matrix addition and store the result in 'result'
+Matrix add_matrices(const Matrix& matrix_1, const Matrix& matrix_2) {
+    // TODO: Implement matrix addition
 }
 
-void multiply_matrices(const double matrix_1[][MAX_SIZE], const double matrix_2[][MAX_SIZE], double result[][MAX_SIZE], int matrix_size) {
-  // TODO: Implement matrix multiplication and store the result in 'result'
+Matrix multiply_matrices(const Matrix& matrix_1, const Matrix& matrix_2) {
+    // TODO: Implement matrix multiplication
 }
 
-void get_diagonal_sum(const double matrix[][MAX_SIZE], int matrix_size) {
-  // TODO: Calculate and print the sum of the diagonal elements
+// Overloaded + operator for matrix addition
+Matrix operator+(const Matrix& matrix_1, const Matrix& matrix_2);
+
+// Overloaded * operator for matrix multiplication
+Matrix operator*(const Matrix& matrix_1, const Matrix& matrix_2);
+
+void get_diagonal_sum(const Matrix& matrix) {
+    // TODO: Calculate and print the sum of the diagonal elements
 }
 
-void swap_matrix_row(double matrix[][MAX_SIZE], int matrix_size, int row1, int row2) {
-  // TODO: Swap the rows 'row1' and 'row2' in the matrix
-  //       Handle invalid row indices
+void swap_matrix_row(Matrix& matrix, int row1, int row2) {
+    // TODO: Swap the rows 'row1' and 'row2' in the matrix
+    //    Handle invalid row indices
 }
 
-int main(int argc, char *argv[]) {
-  double matrix_1[MAX_SIZE][MAX_SIZE], matrix_2[MAX_SIZE][MAX_SIZE];
-  int matrix_size;
+int main(int argc, char* argv[]) {
+    Matrix matrix_1, matrix_2;
+    read_matrix_from_file("matrix.txt", matrix_1, matrix_2);
 
-  read_matrix_from_stream(matrix_1, matrix_2, matrix_size);
-  cout << "print_matrix" << endl;
-  print_matrix(matrix_1, matrix_2, matrix_size);
+    cout << "print_matrix" << endl;
+    print_matrix(matrix_1, matrix_2);
 
-  double add_result[MAX_SIZE][MAX_SIZE];
-  add_matrices(matrix_1, matrix_2, add_result, matrix_size);
-  cout << "add_matrices result:" << endl;
-  print_matrix(add_result, matrix_size);
+    cout << "add_matrices result:" << endl;
+    Matrix add_result_1 = add_matrices(matrix_1, matrix_2);
+    Matrix add_result_2 = matrix_1 + matrix_2;
+    print_matrix(add_result_1);
+    print_matrix(add_result_2);
 
-  double multiply_result[MAX_SIZE][MAX_SIZE];
-  multiply_matrices(matrix_1, matrix_2, multiply_result, matrix_size);
-  cout << "multiply_matrices result:" << endl;
-  print_matrix(multiply_result, matrix_size);
+    cout << "multiply_matrices result:" << endl;
+    Matrix multiply_result_1 = multiply_matrices(matrix_1, matrix_2);
+    Matrix multiply_result_2 = matrix_1 * matrix_2;
+    print_matrix(multiply_result_1);
+    print_matrix(multiply_result_2);
 
-  cout << "get matrix diagonal sum" << endl;
-  get_diagonal_sum(matrix_1, matrix_size);
+    cout << "get matrix diagonal sum" << endl;
+    get_diagonal_sum(matrix_1);
 
-  cout << "swap matrix rows" << endl;
-  swap_matrix_row(matrix_1, matrix_size, 0, 1);
+    cout << "swap matrix rows" << endl;
+    swap_matrix_row(matrix_1, 0, 1);
 
-  return 0;
+    return 0;
 }
