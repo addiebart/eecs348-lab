20f9673c96cdbd56669b1df0a93c4b6aa5a1ef07
Addison Bartelli <addiebart@ku.edu>
2024-11-19 15:38:00 -0600

    done with extra credit code

--- ExtraCredit/main.cpp
+++ ExtraCredit/main.cpp
@@ -1,12 +1,74 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <cmath> // for pow()
 
 using namespace std;
 
+int charToInt(const char& c) {
+    switch (c) { // return doubles as break
+        case '0':
+            return 0;
+        case '1':
+            return 1;
+        case '2':
+            return 2;
+        case '3':
+            return 3;
+        case '4':
+            return 4;
+        case '5':
+            return 5;
+        case '6':
+            return 6;
+        case '7':
+            return 7;
+        case '8':
+            return 8;
+        case '9':
+            return 9;
+        default:
+            return -1;
+    }
+}
+
 double extractNumeric(const string& str) {
     double out;
-    // do stuff
+    int radixIndex = -1;
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == '.') {
+            radixIndex = i;
+            break;
+        }
+    }
+    int wholepart = 0;
+    double fractionalpart = 0;
+    // ".002" will be the same as 0.002. "22." will be the same as 22.0.
+    if (radixIndex == -1) { // no fractional part
+        radixIndex = str.length();
+    } else { // fractional part exists
+        //find the fractional part
+        for (int i = radixIndex + 1; i < str.length(); i++) {
+            int exponent = i - radixIndex;
+            int charint = charToInt(str[i]);
+            if (charint < 0) {
+                return -999999.9;
+            }
+            fractionalpart += (charint * (pow(0.1, exponent)));
+        }
+    }
+    {
+        //find the non fractional part
+        for (int i = radixIndex - 1; i >= 0; i--) {
+            int exponent = radixIndex - i - 1;
+            int charint = charToInt(str[i]);
+            if (charint < 0) {
+                return -999999.99;
+            }
+            wholepart += (charint * (pow(10, exponent)));
+        }
+    }
+    out = wholepart + fractionalpart;
     return out;
 }
 
